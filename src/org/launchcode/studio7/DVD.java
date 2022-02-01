8cdfad43b2c3a570dd1711c8e14217a2af45a63a
61524 <unconfigured@null.spigotmc.org>
2022-01-31 19:33:16 -0600

    studio 7 complete

--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,10 +1,38 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements OpticalDisc{
 
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
+
+    public DVD() {
+        this.discType = "dvd";
+        this.lowSpinRpm = 570;
+        this.highSpinRpm = 1600;
+    }
+
+    public void playVideo() {
+        System.out.println("playVideo() ran successfully");
+    }
+
+    @Override
+    public String read() {
+        System.out.println("read() ran successfully");
+        return contents;
+    }
+
+    @Override
+    public void write(String data) {
+        contents = data;
+        System.out.println("write() ran successfully");
+    }
+
+    @Override
+    public void spin() {
+        System.out.println("spin() ran successfully");
+        System.out.println("A " + discType + " spins at a rate of " + lowSpinRpm + " - " + highSpinRpm + " rpm.");
+    }
 
 }
--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -8,5 +8,17 @@ public class Main {
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
+        CD cd = new CD();
+        DVD dvd = new DVD();
+
+        cd.playAudio();
+        cd.spin();
+        cd.write("123");
+        cd.read();
+
+        dvd.playVideo();
+        dvd.spin();
+        dvd.write("123");
+        dvd.read();
     }
 }
